0db7dcf4a952d9a7089d3ddb3f13184117da933d
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-26 08:38:18 -0500

    Added Questions to Quiz

--- script.js
+++ script.js
@@ -11,3 +11,56 @@ buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+{
+    question: "Who is the strongest Avenger?",
+    answers: {
+        a: "Ironman",
+        b: "Thor",
+        c: "Hulk",
+        d: "Captain America",
+    },
+    correctAnswer: "c"
+},
+{
+    question: "How many Marvel movies are there?",
+    answers: {
+        a: "7",
+        b: "13",
+        c: "23",
+        d: "120",
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Which of these is NOT an inifinity stone?",
+    answers: {
+        a: "Time",
+        b: "Reality",
+        c: "Space",
+        d: "Love",
+    },
+    correctAnswer: "d"
+},
+{
+    question: "What country are Scarlet Witch and Quicksilver from?",
+    answers: {
+        a: "Sokovia",
+        b: "Krakoa",
+        c: "Wakanda",
+        d: "Symkaria",
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Who can lift Thor's Hammer?",
+    answers: {
+        a: "Spiderman",
+        b: "Captain America",
+        c: "Tony Stark",
+        d: "Black Widow",
+    },
+    correctAnswer: "b"
+},
+];
