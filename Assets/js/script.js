d66a7b1d7148f624b3403a42366e4b4ee9bab0f2
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-26 09:48:06 -0500

    Finalized basic quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,39 +1,39 @@
 // Functions
-function buildQuiz(){ 
-    // variable to store the list of possible answers
+function buildQuiz(){
+    // variable to store the HTML output
     const output = [];
-
-    // for each question
+  
+    // for each question...
     myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            // variable to store the list of possible answers
-            const answers = [];
-
-            // and for each available answer
-            for(letter in currentQuestion.answers){
-
-                // add and HTML radio button
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value="${leter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                </label>`
-                );
-            }
-
-            //add this question and its answer to the output
-            output.push(
-                `<div class="questions"> ${currentQuestions.question} </div>
-                <div class="answers'> ${answers.join('')} </div>`
-            );
-            }
+      (currentQuestion, questionNumber) => {
+  
+        // variable to store the list of possible answers
+        const answers = [];
+  
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+  
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
     );
-
-        //combine our output list into one string of HTML and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
+  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
 
 
 function showResults(){
@@ -71,25 +71,18 @@ function showResults(){
   }
 
 // Variables
-const quizContainer = document.getElementById('quizhold');
+const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-const myQuestions = [ ... ];
 
-// Kick things off
-buildQuiz();
-
-// Event listeners
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-{
-    question: "Who is the strongest Avenger?";
+const myQuestions = [ 
+    {
+    question: "Who is the strongest Avenger?",
     answers: {
-        a: "Ironman";
-        b: "Thor";
-        c: "Hulk";
-        d: "Captain America";
+        a: "Ironman",
+        b: "Thor",
+        c: "Hulk",
+        d: "Captain America",
     },
     correctAnswer: "c"
 },
@@ -132,5 +125,11 @@ const myQuestions = [
         d: "Black Widow",
     },
     correctAnswer: "b"
-},
+}, 
 ];
+
+// Kick things off
+buildQuiz();
+
+// Event listeners
+submitButton.addEventListener('click', showResults);
