f341eb75aa65954f9db78fc1a9fa75d566ace390
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-03-26 08:24:46 -0500

    Build Functions in .js

---
+++ script.js
@@ -0,0 +1,13 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('resluts');
+var submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
